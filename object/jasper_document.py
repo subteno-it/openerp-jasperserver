28b2964100d8593069150c9d6a7d1003365c3193
Sebastien LANGE <sebastien.lange@Syleam.fr>
2011-01-17 23:02:41 +0100

    [IMP] Increase attachment field size to 255

--- object/jasper_document.py
+++ object/jasper_document.py
@@ -87,7 +87,7 @@ class jasper_document(osv.osv):
         'mode': fields.selection([('sql','SQL'),('xml','XML')], 'Mode', required=True),
         'before': fields.text('Before', help='This field must be filled with a valid SQL request and will be executed BEFORE the report edition',),
         'after': fields.text('After', help='This field must be filled with a valid SQL request and will be executed AFTER the report edition',),
-        'attachment': fields.char('Save As Attachment Prefix', size=128, help='This is the filename of the attachment used to store the printing result. Keep empty to not save the printed reports. You can use a python expression with the object and time variables.'),
+        'attachment': fields.char('Save As Attachment Prefix', size=255, help='This is the filename of the attachment used to store the printing result. Keep empty to not save the printed reports. You can use a python expression with the object and time variables.'),
         'attachment_use': fields.boolean('Reload from Attachment', help='If you check this, then the second time the user prints with same attachment name, it returns the previous report.'),
         'toolbar': fields.boolean('Hide in toolbar', help='Check this if you want to hide button in toolbar'),
         'param_ids': fields.one2many('jasper.document.parameter', 'document_id', 'Parameters', ),
