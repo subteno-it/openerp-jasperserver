d826f02a66cc49e6b48956b458110b4dad37e210
Damien CRIER <damien.crier@Syleam.fr>
2011-01-20 11:34:58 +0100

    [ADD] add plpgsql function to retrieve translation of a field from its lang and text

---
+++ sql/requete_traduction.sql
@@ -0,0 +1,18 @@
+﻿CREATE OR REPLACE FUNCTION get_trad(langue varchar, typ varchar, nom varchar, texte varchar) RETURNS varchar AS $$
+	DECLARE
+	  resultat varchar;
+	  resultat1 varchar;
+
+        BEGIN
+		IF texte is not null then
+			select into resultat value from ir_translation where lang=langue and type=typ and name=nom and src=texte;
+		ELSE
+			select into resultat value from ir_translation where lang=langue and type=typ and name=nom;
+		END IF;
+		IF NOT FOUND THEN
+			RETURN texte;
+		ELSE
+			RETURN resultat;
+		END IF;
+        END;
+$$ LANGUAGE plpgsql;
