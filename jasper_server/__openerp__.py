a5c4b3d1130d7e68fd05e2087ee59c0695dc799a
Christophe Chauvet <christophe.chauvet@syleam.fr>
2011-03-31 10:40:28 +0200

    [IMP] Update description

--- jasper_server/__openerp__.py
+++ jasper_server/__openerp__.py
@@ -26,14 +26,23 @@
     'name': 'JasperReport Server Interface',
     'version': '6.5.5',
     'category': 'Tools',
-    'description': """This module interface JasperReport Server
+    'description': """This module interface JasperReport Server with OpenERP
+Features:
+- Document source must be in CSV, XML
+- Save document as attachment on object
+- Retrieve attachment if present
+- Launch multiple reports and merge in one printing action
+- Add additionnals parameters (ex from fields function)
+- Affect group on report
+- Use context to display or not the print button (eg: in stock.picking separate per type)
+- Execute SQL query before and after treatement
+- Launch report based on SQL View
 
 This module required library to work properly
 
 # pip install httplib2 (>= 0.6.0)
 # pip install pyPdf (>= 1.13)
-
-    """,
+""",
     'author': 'Everlibre,SYLEAM',
     'website': 'http://www.syleam.fr',
     'depends': [
