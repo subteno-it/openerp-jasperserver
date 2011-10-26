7e68b34bb14710c0c87e07b3fdb51c070963c4c8
Christophe Chauvet <christophe.chauvet@syleam.fr>
2011-10-26 19:56:32 +0200

    [ADD] add new query get_trad to retrieve the real translation, not fuzzy text matching

--- jasper_server/sql/query_translation.sql
+++ jasper_server/sql/query_translation.sql
@@ -2,20 +2,47 @@
 -- Query to translate term in SQL query
 --
 CREATE OR REPLACE FUNCTION get_trad(langue varchar, typ varchar, nom varchar, texte varchar) RETURNS varchar AS $$
-	DECLARE
-	  resultat text;
-	  resultat1 text;
+    DECLARE
+        resultat text;
+        resultat1 text;
+ 
+    BEGIN
+        IF texte is not null then
+            select into resultat value::text from ir_translation where lang=langue and type=typ and name=nom and src=texte;
+        ELSE
+            select into resultat value::text from ir_translation where lang=langue and type=typ and name=nom;
+        END IF;
+        IF NOT FOUND THEN
+            RETURN texte;
+        ELSE
+            RETURN resultat;
+        END IF;
+    END;
+$$ LANGUAGE plpgsql;
 
-        BEGIN
-		IF texte is not null then
-			select into resultat value::text from ir_translation where lang=langue and type=typ and name=nom and src=texte;
-		ELSE
-			select into resultat value::text from ir_translation where lang=langue and type=typ and name=nom;
-		END IF;
-		IF NOT FOUND THEN
-			RETURN texte;
-		ELSE
-			RETURN resultat;
-		END IF;
-        END;
+--
+-- Base translation to there id, better match query.
+--
+CREATE OR REPLACE FUNCTION get_trad(lang varchar, model varchar, field varchar, id integer, defval varchar) RETURNS varchar AS $$
+    DECLARE
+        resultat text;
+
+    BEGIN
+        IF id IS NOT NULL THEN
+            SELECT INTO resultat value::text 
+            FROM  ir_translation 
+            WHERE lang=lang 
+            AND   type='model' 
+            AND   name=model||','||field 
+            AND   res_id=id;
+        ELSE
+            RETURN defval;
+        END IF;
+
+        IF NOT FOUND THEN
+            RETURN defval;
+        ELSE
+            RETURN resultat;
+        END IF;
+    END;
 $$ LANGUAGE plpgsql;
