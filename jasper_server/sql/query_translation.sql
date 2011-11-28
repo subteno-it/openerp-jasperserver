0dcc7a8651be295e36b6e60c17233c9e1ce3e1fe
Christophe Chauvet <christophe.chauvet@syleam.fr>
2011-11-28 09:37:29 +0100

    [IMP] Document function parameters

--- jasper_server/sql/query_translation.sql
+++ jasper_server/sql/query_translation.sql
@@ -1,27 +1,35 @@
 --
 -- Query to translate term in SQL query
 --
-CREATE OR REPLACE FUNCTION get_trad(langue varchar, typ varchar, nom varchar, texte varchar) RETURNS varchar AS $$
-    DECLARE
-        resultat text;
-        resultat1 text;
- 
-    BEGIN
-        IF texte is not null then
-            select into resultat value::text from ir_translation where lang=langue and type=typ and name=nom and src=texte;
-        ELSE
-            select into resultat value::text from ir_translation where lang=langue and type=typ and name=nom;
-        END IF;
-        IF NOT FOUND THEN
-            RETURN texte;
-        ELSE
-            RETURN resultat;
-        END IF;
-    END;
-$$ LANGUAGE plpgsql;
+-- Don't use it in production, doesn't return correct translation
+--
+-- CREATE OR REPLACE FUNCTION get_trad(langue varchar, typ varchar, nom varchar, texte varchar) RETURNS varchar AS $$
+--     DECLARE
+--         resultat text;
+--         resultat1 text;
+--  
+--     BEGIN
+--         IF texte is not null then
+--             select into resultat value::text from ir_translation where lang=langue and type=typ and name=nom and src=texte;
+--         ELSE
+--             select into resultat value::text from ir_translation where lang=langue and type=typ and name=nom;
+--         END IF;
+--         IF NOT FOUND THEN
+--             RETURN texte;
+--         ELSE
+--             RETURN resultat;
+--         END IF;
+--     END;
+-- $$ LANGUAGE plpgsql;
 
 --
 -- Base translation to there id, better match query.
+--
+-- :param lang: ISO code of the language, eg: fr_FR
+-- :param model: OpenERP model, eg: product.template
+-- :param field: field containt the translation, eg: name
+-- :param id: id of the current object
+-- :param defval: Return this value, if no translation found
 --
 CREATE OR REPLACE FUNCTION get_trad(lang varchar, model varchar, field varchar, id integer, defval varchar) RETURNS varchar AS $$
     DECLARE
