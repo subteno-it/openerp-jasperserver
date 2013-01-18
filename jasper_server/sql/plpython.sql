a287e306c225f2e54c4c3382ae097cad59cc0350
Christophe CHAUVET <christophe.chauvet@syleam.fr>
2013-01-18 10:09:39 +0100

    [IMP] Send full traceback in the PostgreSQL Log

--- jasper_server/sql/plpython.sql
+++ jasper_server/sql/plpython.sql
@@ -1,26 +1,37 @@
 --
 -- Add some function
 --
-CREATE OR REPLACE FUNCTION get_field(server text, 
-                                     port integer, 
-                                     login text, 
-                                     password text, 
-                                     database text, 
-                                     model text, 
-                                     obj_id integer, 
-                                     field text)
-RETURNS text AS $BODY$
+CREATE OR REPLACE FUNCTION get_field(
+    server   text,
+    port     integer,
+    login    text,
+    password text,
+    database text,
+    model    text,
+    obj_id   integer,
+    field    text)
+RETURNS text AS 
+$BODY$
 
 if obj_id is None:
     return '0'
 else :
     from oersted import OEClient
 
-    oeclient = OEClient(server, port)
-    oeclient.login(database, login, password)
-    obj = oeclient.create_proxy(database, model)
+    res = {}
+    try:
+        oeclient = OEClient(server, port)
+        oeclient.login(database, login, password)
+        obj = oeclient.create_proxy(database, model)
 
-    res = obj.read(obj_id, [field])
+        res = obj.read(obj_id, [field])
+
+    except AssertionError, e:
+        plpy.error('Authentification error')
+    except Exception, e:
+        import traceback
+        plpy.error(traceback.format_exc())
+        raise
 
     return res[field]
 
@@ -29,3 +40,5 @@ LANGUAGE plpythonu;
 
 ALTER FUNCTION get_field(text, integer, text, text, text, text, integer, text)
   OWNER TO %(db_user)s;
+
+-- vim:ft=pgsql
