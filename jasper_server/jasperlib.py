1e073b76ba85c9ee284099a5e36f7bcd03fc6e3b
Christophe CHAUVET <christophe.chauvet@syleam.fr>
2013-04-01 15:15:26 +0200

    [IMP] Better retrieve error

--- jasper_server/jasperlib.py
+++ jasper_server/jasperlib.py
@@ -23,9 +23,12 @@
 ##############################################################################
 
 from httplib2 import Http
-import socket
-
 from lxml import etree
+from StringIO import StringIO
+
+import socket
+import re
+import email
 
 
 ## Create some exception
@@ -38,6 +41,18 @@ class ServerNotFound(JasperException):
 
 
 class AuthError(JasperException):
+    pass
+
+
+class NotMultipartContent(JasperException):
+    pass
+
+
+class UnknownResponse(JasperException):
+    pass
+
+
+class ServerError(JasperException):
     pass
 
 
@@ -63,23 +78,24 @@ class RequestArgument(etree.ElementBase):
         super(RequestArgument, self).__init__(name=name)
         self.text = value
 
-NSMAP = {
-    'SOAP-ENV': "http://schemas.xmlsoap.org/soap/envelope/",
-    'xsd': "http://www.w3.org/2001/XMLSchema",
-    'xsi': "http://www.w3.org/2001/XMLSchema-instance",
-    'SOAP-ENC': "http://schemas.xmlsoap.org/soap/encoding/",
-    'ns4': "http://www.jaspersoft.com/client",
-}
-
 
 class SoapEnv(object):
+
+    NSMAP = {
+        'SOAP-ENV': "http://schemas.xmlsoap.org/soap/envelope/",
+        'xsd': "http://www.w3.org/2001/XMLSchema",
+        'xsi': "http://www.w3.org/2001/XMLSchema-instance",
+        'SOAP-ENC': "http://schemas.xmlsoap.org/soap/encoding/",
+        'ns4': "http://www.jaspersoft.com/client",
+    }
+
     def __init__(self, wsType='list', action=''):
-        self.soap = etree.Element('{%s}Envelope' % NSMAP['SOAP-ENV'], nsmap=NSMAP)
-        body = etree.Element('{%s}Body' % NSMAP['SOAP-ENV'])
-        action_body = etree.Element('{%s}%s' % (NSMAP['ns4'], wsType))
-        action_body.text = action
-        body.append(action_body)
-        self.soap.append(body)
+        self.soap = etree.Element('{%s}Envelope' % self.NSMAP['SOAP-ENV'], nsmap=self.NSMAP)
+        body = etree.SubElement(self.soap, '{%s}Body' % self.NSMAP['SOAP-ENV'])
+        action_body = etree.SubElement(body, '{%s}%s' % (self.NSMAP['ns4'], wsType))
+        request = etree.SubElement(action_body, 'request')
+        request.set('{%s}type' % (self.NSMAP['xsi'],), 'xsd:string')
+        request.text = action
 
     def output(self,):
         return etree.tostring(self.soap, pretty_print=True)
@@ -98,6 +114,7 @@ class Jasper(object):
         self.user = user
         self.pwd = pwd
         self.repo = 'jasperserver/services/repository'
+        self.uri = 'http://%s:%s/%s' % (self.host, self.port, self.repo)
 
         self.headers = {
             'Content-type': 'text/xml',
@@ -113,14 +130,13 @@ class Jasper(object):
         self.cnx.add_credentials(self.user, self.pwd)
 
         # We must simulate a request if we want to check the auth is correct
-        uri = 'http://%s:%s/%s' % (self.host, self.port, self.repo)
 
         # Generate a soap query to verify the authentification
         rq = Request(operationName='list', locale='fr_FR')
         rq.append(RequestRD('folder', '', '/'))
-        self.body = SoapEnv('runReport', etree.tostring(rq)).output()
+        self.body = SoapEnv('list', etree.tostring(rq)).output()
         try:
-            res, content = self.cnx.request(uri, 'POST', self.body, self.headers)
+            res, content = self.cnx.request(self.uri, 'POST', self.body, self.headers)
         except socket.error:
             raise ServerNotFound('Server not found')
 
@@ -131,15 +147,80 @@ class Jasper(object):
 
         return True
 
-    def create_request(self, arguments=None,):
+    def send(self, soapQuery=''):
+        try:
+            res, content = self.cnx.request(self.uri, 'POST', soapQuery, self.headers)
+        except socket.error:
+            raise ServerNotFound('Server not found')
+
+        if res.get('status', '200') == '401':
+            raise AuthError('Authentification Failed !')
+        elif res.get('status', '200') != '200':
+            return False
+
+        try:
+            return self.parseMultipart(content)
+        except NotMultipartContent:
+            fp = StringIO(content)
+            tree = etree.parse(fp)
+            fp.close()
+            r = tree.xpath('//runReportReturn')
+            if not r:
+                raise UnknownResponse(content)
+
+            fp = StringIO(r[0].text.encode('utf-8'))
+            tree = etree.parse(fp)
+            fp.close()
+
+            raise ServerError('[' + tree.xpath('//returnCode')[0].text.encode('utf-8') + ']' + tree.xpath('//returnMessage')[0].text.encode('utf-8'))
+
+    def create_request(self, operation='list', wsType='', uri='/', name='', arguments=None, params=None):
         if arguments is None:
             arguments = {}
 
-        rq = Request(locale='fr_FR')
+        if params is None:
+            params = {}
+
+        rq = Request(operationName=operation, locale='fr_FR')
         for k in arguments:
             rq.append(RequestArgument(name=k, value=arguments[k]))
-        rq.append(RequestRD('runReport', '', '/openerp/bases'))
+
+        # Add resource descriptor
+        rd = RequestRD(operation, name, uri)
+        label = etree.SubElement(rd, 'label')
+        label.text = 'null'
+
+        # Add query parameters
+        for k, v in params.items():
+            p = etree.SubElement(rd, 'parameter', name=k)
+            p.text = str(v)
+
+        rq.append(rd)
         return etree.tostring(rq, pretty_print=True)
+
+    def run_report(self, uri='/', output='PDF', params=None):
+        """
+        Launch a runReport in Jasper
+        """
+        args = {
+            'RUN_OUTPUT_FORMAT': output,
+            'PAGE': '0',
+        }
+
+        return self.create_request(operation='runReport', wsType='reportUnit', uri=uri, arguments=args, params=params)
+
+    @staticmethod
+    def parseMultipart(res):
+        srch = re.search(r'----=[^\r\n]*', res)
+        if srch is None:
+            raise NotMultipartContent()
+
+        boundary = srch.group()
+        res = " \n" + res
+        res = "Content-Type: multipart/alternative; boundary=%s\n%s" % (boundary, res)
+        message = email.message_from_string(res)
+        attachment = message.get_payload()[1]
+        return {'content-type': attachment.get_content_type(), 'data': attachment.get_payload()}
 
     def log_last_request(self,):
         """
@@ -156,12 +237,33 @@ if __name__ == '__main__':
     except AuthError:
         print 'Error, Authentification failed for %s/%s' % (js.user, js.pwd)
 
-    args = {
-        'RUN_OUTPUT_FORMAT': 'PDF',
-        'PAGE': '0',
+    params = {
+        'OERP_COMPANY_ID': 1,
+        'OERP_ACTIVE_ID': 1,
+        'OERP_ACTIVE_IDS': '1,2,3',
     }
-    x = js.create_request(arguments=args)
-    print x
-    print SoapEnv('list', x).output()
+    envelop = js.run_report(uri='/reports/samples/AllAccounts', output='PDF', params=params)
+    try:
+        a = js.send(SoapEnv('runReport', envelop).output())
+        f = file('AllAccounts.pdf', 'w')
+        f.write(a['data'])
+        f.close()
+    except ServerNotFound:
+        print 'Error, server not found %s %d' % (js.host, js.port)
+    except AuthError:
+        print 'Error, Authentification failed for %s/%s' % (js.user, js.pwd)
+
+    envelop = js.run_report(uri='/reports/samples/AllAccounts2', output='XLS', params=params)
+    try:
+        a = js.send(SoapEnv('runReport', envelop).output())
+        f = file('AllAccounts.xls', 'w')
+        f.write(a['data'])
+        f.close()
+    except ServerNotFound:
+        print 'Error, server not found %s %d' % (js.host, js.port)
+    except AuthError:
+        print 'Error, Authentification failed for %s/%s' % (js.user, js.pwd)
+    except ServerError, e:
+        print str(e)
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
